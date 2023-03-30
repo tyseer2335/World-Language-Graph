b41e39c31c9c926f68662053918ebad3c155aba4
Tyseer <tyseer2334@gmail.com>
2023-03-30 03:40:36 -0400

    fixed visuals

--- generate_graph.py
+++ generate_graph.py
@@ -4,10 +4,10 @@ from pyvis.network import Network
 from csv_reading import read_csv
 
 
-def generate_visuals(graph: LanguageGraph):
+def generate_visuals(graph: LanguageGraph, name: str) -> None:
     """
     Takes in a LanguageGraph generated using our CSV data and, generates a visual repersentation of the graph using the
-    Pyvis Library
+    Pyvis Library. Also takes in a string to name the generated file
     """
     net = Network(height="1250px", width="100%", bgcolor="#03001C", font_color="#FFF2F2")
 
@@ -29,17 +29,17 @@ def generate_visuals(graph: LanguageGraph):
             net.add_node(n_id=graph.get_language()[x].name,
                          title=graph.get_language()[x].name,
                          color="#FFDEB4",
-                         size=15,
+                         size=20,
                          label=graph.get_language()[x].name)
 
     # Add all the Edges
-    edge_color = "#F0E3E3"
+    edge_color = "#2F58CD"
     for k in graph.get_language():
         for j in graph.get_language()[k].neighbours:
-            net.add_edge(graph.get_language()[k].name, j.name, color=edge_color, width=2)
+            net.add_edge(graph.get_language()[k].name, j.name, color=edge_color, width=3)
 
-    net.show('completed_visuals.html')
+    net.show(name + '.html')
 
 # Sample Function Call
 # languages_graph = read_csv("csv/relevant_genus_languages.csv", "csv/creole_languages.csv")
-# generate_visuals(languages_graph)
+# generate_visuals(languages_graph, "completed_visuals")
